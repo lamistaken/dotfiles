cefa0a17402e384516eb99f7d9a47fd7bdf574b0
Trung Lam <lam@lamtrung.com>
2025-05-30 01:06:29 -0700

    Maybe fix scaling when unplugging monitor

--- shared/dot-config/hypr/UserScripts/laptoplid.sh
+++ shared/dot-config/hypr/UserScripts/laptoplid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 == "open" ]]; then
-    hyprctl keyword monitor "eDP-1,preferred,auto,2" 
-elif [[ "$( hyprctl monitors | rg Monitor | wc -l )" -gt 1 ]]; then
-    hyprctl keyword monitor "eDP-1,disable" 
+    hyprctl keyword monitor "eDP-1,preferred,auto,1"
+elif [[ "$(hyprctl monitors | rg Monitor | wc -l)" -gt 1 ]]; then
+    hyprctl keyword monitor "eDP-1,disable"
 fi
