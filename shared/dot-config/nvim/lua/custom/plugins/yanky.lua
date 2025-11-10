0a3bc5d672d65482fe994af9f2405211a5f125da
Trung Lam <lam@lamtrung.com>
2025-11-10 13:58:23 -0800

    Add yank commands

--- shared/dot-config/nvim/lua/custom/plugins/yanky.lua
+++ shared/dot-config/nvim/lua/custom/plugins/yanky.lua
@@ -9,6 +9,22 @@ return {
       { '<leader>yy', '"+yy', mode = { 'n', 'v', 'x' }, desc = 'Yank line to clipboard' },
       { '<leader>yp', '<Plug>(YankyPutAfter)', mode = { 'n', 'v', 'x' }, desc = 'Put Text After Cursor' },
       { '<leader>yP', '<Plug>(YankyPutBefore)', mode = { 'n', 'v', 'x' }, desc = 'Put Text Before Cursor' },
+      {
+        '<leader>yf',
+        function()
+          local filepath = vim.fn.expand '%:.'
+          vim.fn.setreg('+', filepath)
+        end,
+        desc = 'Yank relative file path to clipboard',
+      },
+      {
+        '<leader>yF',
+        function()
+          local filepath = vim.fn.expand '%:p'
+          vim.fn.setreg('+', filepath)
+        end,
+        desc = 'Yank absolute file path to clipboard',
+      },
     },
   },
 }
