1243514b241af30099fe4309c9f1ad1ffa8cd410
Trung Lam <lam@lamtrung.com>
2026-01-31 14:24:17 -0800

    delete hypr

--- shared/dot-config/hypr/UserScripts/laptoplid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ $1 == "open" ]]; then
-    hyprctl keyword monitor "eDP-1,preferred,auto,1"
-elif [[ "$(hyprctl monitors | rg Monitor | wc -l)" -gt 1 ]]; then
-    hyprctl keyword monitor "eDP-1,disable"
-fi
